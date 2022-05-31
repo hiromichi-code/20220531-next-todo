247593952c10129ec57fe3bfc631a3ec408d27c7
Hiro <hiromichi.s.617@gmail.com>
2022-05-31 19:52:51 +0900

    second

--- pages/Login.js
+++ pages/Login.js
@@ -19,28 +19,28 @@ function Login(props) {
 	}, [props.history]);
 
 	return (
-		<div className={`bg-red-300 h-screen flex items-center justify-center`}>
+		<div className={`bg-sky-500 h-screen flex items-center justify-center`}>
 			<div
 				className={`bg-white h-full lg:h-1/2 w-full lg:w-1/5 flex flex-col items-center justify-center`}>
-				<h1 className={`text-2xl text-red-300`}>
+				<h1 className={`text-2xl text-sky-500`}>
 					{isLogin ? "Login" : "Register"}
 				</h1>
-				<small className={`text-red-300 mt-5`}>Email</small>
+				<small className={`text-sky-500 mt-5`}>Email</small>
 				<input
 					value={email}
 					onChange={(e) => setEmail(e.target.value)}
-					className={`border-2 border-red-300 pl-1`}
+					className={`border-2 border-sky-500 pl-1`}
 					placeholder="input Email"
 				/>
-				<small className={`text-red-300 mt-5`}>Password</small>
+				<small className={`text-sky-500 mt-5`}>Password</small>
 				<input
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
-					className={`border-2 border-red-300 pl-1`}
+					className={`border-2 border-sky-500 pl-1`}
 					placeholder="input Password"
 				/>
 				<button
-					className={`bg-red-300 text-white py-1 px-2 mt-8`}
+					className={`bg-sky-500 text-white py-1 px-2 mt-8`}
 					onClick={
 						isLogin
 							? async () => {
@@ -64,7 +64,7 @@ function Login(props) {
 				</button>
 				<p
 					onClick={(e) => setIsLogin(!isLogin)}
-					className={`text-red-300 cursor-pointer mt-5 border-b-2 border-red-300`}>
+					className={`text-sky-500 cursor-pointer mt-5 border-b-2 border-sky-500`}>
 					{isLogin ? "Create new account？" : "Back to Login"}
 				</p>
 			</div>
